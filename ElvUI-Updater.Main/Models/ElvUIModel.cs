1e14949deb9d30b2dd9cb3ce2f7f52a64ef57a50
ArjanDeo <arjandeo@bluecomet-it.com>
2023-10-12 21:56:41 +0100

    Add project files.

---
+++ ElvUI-Updater.Main/Models/ElvUIModel.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+
+namespace ElvUI_Updater.Main.Models
+{	public class ElvUIModel
+	{
+		[JsonProperty("id")]
+		public int Id { get; set; }
+
+		[JsonProperty("slug")]
+		public string Slug { get; set; }
+
+		[JsonProperty("author")]
+		public string Author { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("url")]
+		public string Url { get; set; }
+
+		[JsonProperty("version")]
+		public string Version { get; set; }
+
+		[JsonProperty("changelog_url")]
+		public string ChangelogUrl { get; set; }
+
+		[JsonProperty("ticket_url")]
+		public string TicketUrl { get; set; }
+
+		[JsonProperty("git_url")]
+		public string GitUrl { get; set; }
+
+		[JsonProperty("patch")]
+		public List<string> Patch { get; set; }
+
+		[JsonProperty("last_update")]
+		public string LastUpdate { get; set; }
+
+		[JsonProperty("web_url")]
+		public string WebUrl { get; set; }
+
+		[JsonProperty("donate_url")]
+		public string DonateUrl { get; set; }
+
+		[JsonProperty("small_desc")]
+		public string SmallDesc { get; set; }
+
+		[JsonProperty("screenshot_url")]
+		public string ScreenshotUrl { get; set; }
+
+		[JsonProperty("directories")]
+		public List<string> Directories { get; set; }
+	}
+}
---
+++ ElvUI-Updater.Main/Program.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using ElvUI_Updater.Main.Models;
+
+namespace ElvUI_Updater.Main
+{
+	internal class Settings
+	{
+		public string AddonsFolderPath { get; set; }
+
+		public void SaveSettings()
+		{
+			File.WriteAllText("settings.txt", AddonsFolderPath);
+		}
+
+		public bool LoadSettings()
+		{
+			if (File.Exists("settings.txt"))
+			{
+				AddonsFolderPath = File.ReadAllText("settings.txt");
+				return true;
+			}
+			return false;
+		}
+	}
+	internal class Program
+	{
+		static readonly HttpClient client = new HttpClient();
+
+		static async Task Main()
+		{
+			Settings settings = new Settings();
+
+			if (!settings.LoadSettings())
+			{
+				Console.WriteLine("Please enter the path to your addons folder:");
+				string addonsPath = Console.ReadLine();
+
+				settings.AddonsFolderPath = addonsPath;
+				settings.SaveSettings();
+			}
+
+			try
+			{
+				await GetElvUIAPIAsync(settings.AddonsFolderPath);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("An exception occurred: " + ex.Message);
+			}
+		}
+
+		public static async Task GetElvUIAPIAsync(string addonsPath)
+		{
+			try
+			{
+				HttpResponseMessage response = await client.GetAsync("https://api.tukui.org/v1/addon/elvui");
+				response.EnsureSuccessStatusCode();
+				string responseBody = await response.Content.ReadAsStringAsync();
+				var x = JsonConvert.DeserializeObject<ElvUIModel>(responseBody);
+				await DownloadAndExtractAddonAsync(addonsPath, x.Url, x.Version);
+			}
+			catch (HttpRequestException ex)
+			{
+				Console.WriteLine("HTTP request exception: " + ex.Message);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("An exception occurred: " + ex.Message);
+			}
+		}
+		public static async Task DownloadAndExtractAddonAsync(string addonsPath, string url, string v)
+		{
+			HttpResponseMessage response = await client.GetAsync(url);
+			if (response.IsSuccessStatusCode)
+			{
+
+				byte[] fileBytes = await response.Content.ReadAsByteArrayAsync();
+
+				string line = File.ReadLines($"{addonsPath}/ElvUI/ElvUI_Mainline.toc").Skip(3).Take(1).First();
+				bool isSameVersion = line.Contains(v);
+				if (isSameVersion)
+				{
+					await Console.Out.WriteLineAsync($"ElvUI is up to date. (version {v})");
+					Console.ReadKey();
+				}
+				else
+				{
+					File.WriteAllBytes($"./elvui-{v}.zip", fileBytes);
+					System.IO.Compression.ZipFile.ExtractToDirectory($"./elvui-{v}.zip", addonsPath, System.Text.Encoding.UTF8, true);
+					Console.WriteLine("File downloaded successfully.");
+				}
+			}
+			else
+			{
+				Console.WriteLine($"Failed to download file. Status code: {response.StatusCode}");
+			}
+		}
+	}
+
+}
