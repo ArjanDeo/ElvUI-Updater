636749e82a3e16ab440c22e15e68051df8498b00
Arjan Deo <Arjan@Jays-MacBook-Air.local>
2026-08-06 16:19:44 +0100

    Switched to clangd and implemented basic settings.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
+#include "Settings.hpp"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    Settings settings;
+    settings.load();
+
+    if (!Settings::isValidWowDirectory(settings.getWowDirectory()))
+    {
+        std::string input;
+
+        while (true)
+        {
+            std::cout << "WoW installation directory not found or invalid.\n";
+            std::cout << "Enter the path to your World of Warcraft folder: ";
+            std::getline(std::cin, input);
+
+            if (Settings::isValidWowDirectory(input))
+            {
+                settings.setWowDirectory(input);
+                settings.save();
+                break;
+            }
+
+            std::cout << "That doesn't look like a valid WoW directory "
+                          "(missing Interface/AddOns). Try again.\n\n";
+        }
+    }
+
+    std::cout << "Using WoW directory: " << settings.getWowDirectory() << "\n";
+    
+
     return 0;
 }
